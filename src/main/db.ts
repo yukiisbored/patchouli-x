a2f06ce926e36617f383b892de225aef836fc590
Yuki Langley <hi@yukiisbo.red>
2024-06-08 16:05:15 +0200

    fix(db): make migrations work

--- src/main/db.ts
+++ src/main/db.ts
@@ -4,6 +4,7 @@ import { mkdirSync } from 'fs'
 import { join } from 'path'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import { migrate as baseMigrate } from 'drizzle-orm/better-sqlite3/migrator'
+import { is } from '@electron-toolkit/utils'
 
 const dataPath = join(app.getPath('userData'), 'Patchouli Data')
 mkdirSync(dataPath, { recursive: true })
@@ -11,13 +12,13 @@ mkdirSync(dataPath, { recursive: true })
 const dbPath = join(dataPath, 'data.sqlite3')
 const sqlite = new Database(dbPath)
 
-// TODO: Figure out how to deliver the drizzle folder that is usable by the migrator.
-const migrationsFolder = join(__dirname, '../../resources/drizzle')
+const migrationsFolder = is.dev
+  ? join(__dirname, '../../resources/drizzle')
+  : join(process.resourcesPath, 'app.asar.unpacked', 'resources', 'drizzle')
 
 export const db = drizzle(sqlite)
 
 export async function migrate(): Promise<void> {
-  console.log(migrationsFolder)
   baseMigrate(db, {
     migrationsFolder
   })
