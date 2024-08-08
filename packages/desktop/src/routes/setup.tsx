339d46596e3e0c0e07ae3e0f05704461fe6879cb
Yuki Langley <hi@yukiisbo.red>
2024-08-08 23:25:25 +0200

    Improve onboarding screen

--- packages/desktop/src/routes/setup.tsx
+++ packages/desktop/src/routes/setup.tsx
@@ -1,7 +1,21 @@
 import { trpc } from '@/trpc'
-import { Button, Flex, Heading, Spacer, Stack, Text } from '@chakra-ui/react'
+import {
+  Button,
+  Heading,
+  Spacer,
+  Stack,
+  Text,
+  Image,
+  Card,
+  CardBody,
+  HStack
+} from '@chakra-ui/react'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { message, open } from '@tauri-apps/api/dialog'
+import logo from '@/assets/icon.png'
+import { IconBulb } from '@tabler/icons-react'
+import { getVersion } from '@tauri-apps/api/app'
+import { useCallback, useEffect, useState } from 'react'
 
 export const Route = createFileRoute('/setup')({
   component: Setup
@@ -10,6 +24,7 @@ export const Route = createFileRoute('/setup')({
 function Setup() {
   const navigate = useNavigate()
   const mutation = trpc.system.configure.useMutation()
+  const [version, setVersion] = useState<string | null>(null)
 
   async function onClick() {
     const selected = await open({
@@ -34,29 +49,54 @@ function Setup() {
     })
   }
 
+  const fetchInformation = useCallback(async () => {
+    setVersion(await getVersion())
+  }, [])
+
+  useEffect(() => {
+    fetchInformation()
+  })
+
   return (
-    <Flex
-      direction="column"
-      alignItems="center"
-      justifyContent="space-between"
-      height="100vh"
+    <Stack
+      mx="auto"
+      h="100vh"
+      maxW={600}
+      align="center"
+      justify="space-between"
+      background="white"
+      padding={8}
     >
       <Spacer />
-      <Stack
-        maxW={600}
-        align="center"
-        background="white"
-        padding={8}
-        borderRadius="md"
-      >
-        <Heading size="xl">Patchouli</Heading>
-        <Text mt={4}>We need a place to store your darlings.</Text>
-        <Text>Please choose the location of your personal vault.</Text>
-        <Button mt={4} onClick={onClick}>
+      <Stack>
+        <Stack>
+          <Image w={32} src={logo} />
+          <Heading size="2xl">Patchouli</Heading>
+          {version && <Text>Version {version}</Text>}
+        </Stack>
+
+        <Stack mt={4}>
+          <Text>To begin, we need a place to store your darlings.</Text>
+          <Text>Please choose the location of your personal vault.</Text>
+        </Stack>
+
+        <Button mt={6} onClick={onClick} size="lg">
           Browse
         </Button>
+
+        <Card mt={4} bg="green.100">
+          <CardBody>
+            <HStack align="flex-start">
+              <IconBulb />
+              <Text fontWeight="bold">Tip:</Text>
+              <Text>
+                You can put your vault in your cloud storage to sync between
+                devices.
+              </Text>
+            </HStack>
+          </CardBody>
+        </Card>
       </Stack>
-      <Spacer />
-    </Flex>
+    </Stack>
   )
 }
